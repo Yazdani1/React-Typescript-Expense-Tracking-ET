a55ee6735fb6eea46de7ddb56c37a2d8dcada4c3
Yazdani Chowdhury <yaz4dani@gmail.com>
2023-08-13 00:24:49 +0300

    WIP

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,27 +1,27 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
-import "./App.css";
-import { UserProvider } from "./contextapi/UserContext";
-import SignIn from "./pages/Auth/SignIn";
-import SignUp from "./pages/Auth/SignUp";
-import ExpenseBook from "./pages/ExpenseBook/ExpenseBook";
-import SecureLayout from "./layouts/SecureLayout";
-import Admin from "./pages/Admin/Admin";
-import AdminSecureLayout from "./layouts/AdminSecureLayout";
-import Profile from "./pages/SubscriberProfile/Profile";
-import ExpenseBookDetails from "./pages/ExpenseBookDetails/ExpenseBookDetails";
-import UserListPagination from "./pages/Admin/UserListPagination";
-import HomeRentalPost from "./pages/HomeRentalApp/HomeRentalPost";
-import NationalId from "./pages/NationalId/NationalId";
-import Income from "./pages/Income/Income";
-import HomeRentalOfflineData from "./pages/HomeRentalOfflineData/HomeRentalOfflineData";
+import './App.css';
+import { UserProvider } from './contextapi/UserContext';
+import SignIn from './pages/Auth/SignIn';
+import SignUp from './pages/Auth/SignUp';
+import ExpenseBook from './pages/ExpenseBook/ExpenseBook';
+import SecureLayout from './layouts/SecureLayout';
+import Admin from './pages/Admin/Admin';
+import AdminSecureLayout from './layouts/AdminSecureLayout';
+import Profile from './pages/SubscriberProfile/Profile';
+import ExpenseBookDetails from './pages/ExpenseBookDetails/ExpenseBookDetails';
+import UserListPagination from './pages/Admin/UserListPagination';
+import HomeRentalPost from './pages/HomeRentalApp/HomeRentalPost';
+import NationalId from './pages/NationalId/NationalId';
+import Income from './pages/Income/Income';
+import HomeRentalOfflineData from './pages/HomeRentalOfflineData/HomeRentalOfflineData';
 //Context API
-import { UserProfileDetailsProvider } from "./contextapi/UserProfileDetailsContext";
-import { UserContextCookieProvider } from "./contextapi/UserContextCookies";
-import { HomeRentalProvider } from "./contextapi/HomeRentalContext";
-import { IncomeRecordProvider } from "./contextapi/IncomeRecordContext";
-import { UserProtectedRouteProvider } from "./contextapi/UserProtectedRouteContext";
+import { UserProfileDetailsProvider } from './contextapi/UserProfileDetailsContext';
+import { UserContextCookieProvider } from './contextapi/UserContextCookies';
+import { HomeRentalProvider } from './contextapi/HomeRentalContext';
+import { IncomeRecordProvider } from './contextapi/IncomeRecordContext';
+import { UserProtectedRouteProvider } from './contextapi/UserProtectedRouteContext';
 
 const App = () => {
   return (
@@ -35,10 +35,7 @@ const App = () => {
                   <Routes>
                     <Route path="/" element={<SignIn />} />
                     <Route path="/signup" element={<SignUp />} />
-                    <Route
-                      path="/search-nationalid-details"
-                      element={<NationalId />}
-                    />
+                    <Route path="/search-nationalid-details" element={<NationalId />} />
 
                     {/* Protected route for subscriber*/}
 
