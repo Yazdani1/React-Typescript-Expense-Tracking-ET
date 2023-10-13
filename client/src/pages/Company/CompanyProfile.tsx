acc876dffab7bc5600cb46e0912308907b2a109a
yazdani1 <yaz4dani@gmail.com>
2023-10-13 23:30:21 +0200

    WIP

--- client/src/pages/Company/CompanyProfile.tsx
+++ client/src/pages/Company/CompanyProfile.tsx
@@ -4,17 +4,24 @@ import CompanyPageLayout from '../../layouts/CompanyPageLayout';
 import CardLayout from '../../components/CardLayout/CardLayout';
 
 const CompanyProfile = () => {
-  return (
-    <CompanyPageLayout>
-      <CardLayout>
-        <div>
-          <h2>Company Profile</h2>
-          <p>Test project</p>
-        </div>
+	return (
+		<CompanyPageLayout>
+			<CardLayout>
+				<div>
+					<h2>Company Profile</h2>
+					<p>Test project</p>
+				</div>
+			</CardLayout>
 
-      </CardLayout>
-    </CompanyPageLayout>
-  );
+			<CardLayout>
+				<div style={{ margin: '30px' }}>
+					<h2>Company Profile</h2>
+					<p>Test project</p>
+					<p>Test project</p>
+				</div>
+			</CardLayout>
+		</CompanyPageLayout>
+	);
 };
 
 export default CompanyProfile;
