d90ce9decd6d0fe1088942dd04daa9ef8726cd8b
yazdani1 <yaz4dani@gmail.com>
2023-10-14 23:06:40 +0200

    WIP

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -16,117 +16,159 @@ import { useUserContext } from '../../contextapi/UserContextCookies';
 import { UserProtectedRouteContext } from '../../contextapi/UserProtectedRouteContext';
 
 const Navbar = () => {
-  let navigate = useNavigate();
-  //Context API
+	let navigate = useNavigate();
+	//Context API
 
-  const { clearJobWishList } = useJobWishListContext();
+	const { clearJobWishList } = useJobWishListContext();
 
-  // to use redux toolkit
-  const userProfileDetails = useSelector((state: any) => state.user.currentUser);
-  const dispatch = useDispatch();
+	// to use redux toolkit
+	const userProfileDetails = useSelector(
+		(state: any) => state.user.currentUser
+	);
+	const dispatch = useDispatch();
 
-  /////////////////////////////////////////////////////////////////////
-  // All these are old context api that i used.
-  // const [state, setState] = useContext(UserContext);
-  // const { userProfileDetails, updateUserProfileDetails } = useContext(
-  //   UserProfileDetailsContext
-  // );
-  // Context API to show user protected route. But this context api is used to show protected route
-  // Context api with cookies
-  const { user, logout } = useUserContext();
-  /////////////////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////
+	// All these are old context api that i used.
+	// const [state, setState] = useContext(UserContext);
+	// const { userProfileDetails, updateUserProfileDetails } = useContext(
+	//   UserProfileDetailsContext
+	// );
 
-  const [userInfo, setUserInfo]: any = useContext(UserProtectedRouteContext);
+	// Context API to show user protected route. But this context api is used to show protected route
+	// Context api with cookies
 
-  // const logOut = () => {
-  //   window.localStorage.removeItem("tokenLogin");
-  //   window.localStorage.removeItem("token");
-  //   navigate("/");
-  //   // setState("");
-  // };
+	const { user, logout } = useUserContext();
 
-  const handleLogout = () => {
-    dispatch(logOut());
-    window.localStorage.removeItem('tokenLogin');
-    window.localStorage.removeItem('token');
-    window.localStorage.removeItem('userInforProtectedRoute');
-    window.localStorage.removeItem('userData');
+	/////////////////////////////////////////////////////////////////////
 
-    setUserInfo('');
-    navigate('/signin');
-    clearJobWishList();
-    logout();
-  };
+	const [userInfo, setUserInfo]: any = useContext(UserProtectedRouteContext);
 
-  return (
-    <nav className={navbarStyle.navbarContainer}>
-      <ul>
-        <li>Redux:{userProfileDetails?.name}||</li>
-        <li>{userProfileDetails?.points} Points</li>
-        <li>{userProfileDetails?.role}</li>
-        <Link to={'/'} style={{ textDecoration: 'none', color: 'white' }}>
-          <li>Home</li>
-        </Link>
-        <li className="nav-item">
-          <Link to={'/dashboard'} style={{ textDecoration: 'none', color: 'white' }}></Link>
-        </li>
-        <li className="nav-item">
-          <Link to={'/dashboard'} style={{ textDecoration: 'none', color: 'white' }}>
-            Expense Book
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to={'/courses'} style={{ textDecoration: 'none', color: 'white' }}>
-            Courses
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to={'/income-record'} style={{ textDecoration: 'none', color: 'white' }}>
-            Income Record
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to={'/home-rental-offline-data'} style={{ textDecoration: 'none', color: 'white' }}>
-            Home Rental Offline Data
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to={'/profile'} style={{ textDecoration: 'none', color: 'white' }}>
-            Profile
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to={'/enroled-courses'} style={{ textDecoration: 'none', color: 'white' }}>
-            Enroled Courses
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to={'/job-match'} style={{ textDecoration: 'none', color: 'white' }}>
-            Job Match
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to={'/job-wishlist'} style={{ textDecoration: 'none', color: 'white' }}>
-            Job Wishlist
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to={'/job-application'} style={{ textDecoration: 'none', color: 'white' }}>
-            Job Application
-          </Link>
-        </li>
+	// const logOut = () => {
+	//   window.localStorage.removeItem("tokenLogin");
+	//   window.localStorage.removeItem("token");
+	//   navigate("/");
+	//   // setState("");
+	// };
 
-        <li className="nav-item">
-          <Link to={'/home-rental'} style={{ textDecoration: 'none', color: 'white' }}>
-            Home Rental
-          </Link>
-        </li>
-        <li className="nav-item" onClick={handleLogout} style={{ cursor: 'pointer' }}>
-          Log Out
-        </li>
-      </ul>
-    </nav>
-  );
+	const handleLogout = () => {
+		dispatch(logOut());
+		window.localStorage.removeItem('tokenLogin');
+		window.localStorage.removeItem('token');
+		window.localStorage.removeItem('userInforProtectedRoute');
+		window.localStorage.removeItem('userData');
+
+		setUserInfo('');
+		navigate('/signin');
+		clearJobWishList();
+		logout();
+	};
+
+	return (
+		<nav className={navbarStyle.navbarContainer}>
+			<ul>
+				<li>Redux:{userProfileDetails?.name}||</li>
+				<li>{userProfileDetails?.points} Points</li>
+				<li>{userProfileDetails?.role}</li>
+				<Link to={'/'} style={{ textDecoration: 'none', color: 'white' }}>
+					<li>Home</li>
+				</Link>
+				<li className='nav-item'>
+					<Link
+						to={'/dashboard'}
+						style={{ textDecoration: 'none', color: 'white' }}
+					></Link>
+				</li>
+				<li className='nav-item'>
+					<Link
+						to={'/dashboard'}
+						style={{ textDecoration: 'none', color: 'white' }}
+					>
+						Expense Book
+					</Link>
+				</li>
+				<li className='nav-item'>
+					<Link
+						to={'/courses'}
+						style={{ textDecoration: 'none', color: 'white' }}
+					>
+						Courses
+					</Link>
+				</li>
+				<li className='nav-item'>
+					<Link
+						to={'/income-record'}
+						style={{ textDecoration: 'none', color: 'white' }}
+					>
+						Income Record
+					</Link>
+				</li>
+				<li className='nav-item'>
+					<Link
+						to={'/home-rental-offline-data'}
+						style={{ textDecoration: 'none', color: 'white' }}
+					>
+						Home Rental Offline Data
+					</Link>
+				</li>
+				<li className='nav-item'>
+					<Link
+						to={'/profile'}
+						style={{ textDecoration: 'none', color: 'white' }}
+					>
+						Profile
+					</Link>
+				</li>
+				<li className='nav-item'>
+					<Link
+						to={'/enroled-courses'}
+						style={{ textDecoration: 'none', color: 'white' }}
+					>
+						Enroled Courses
+					</Link>
+				</li>
+				<li className='nav-item'>
+					<Link
+						to={'/job-match'}
+						style={{ textDecoration: 'none', color: 'white' }}
+					>
+						Job Match
+					</Link>
+				</li>
+				<li className='nav-item'>
+					<Link
+						to={'/job-wishlist'}
+						style={{ textDecoration: 'none', color: 'white' }}
+					>
+						Job Wishlist
+					</Link>
+				</li>
+				<li className='nav-item'>
+					<Link
+						to={'/job-application'}
+						style={{ textDecoration: 'none', color: 'white' }}
+					>
+						Job Application
+					</Link>
+				</li>
+
+				<li className='nav-item'>
+					<Link
+						to={'/home-rental'}
+						style={{ textDecoration: 'none', color: 'white' }}
+					>
+						Home Rental
+					</Link>
+				</li>
+				<li
+					className='nav-item'
+					onClick={handleLogout}
+					style={{ cursor: 'pointer' }}
+				>
+					Log Out
+				</li>
+			</ul>
+		</nav>
+	);
 };
 
 export default Navbar;
--- client/src/pages/SubscriberProfile/Profile.tsx
+++ client/src/pages/SubscriberProfile/Profile.tsx
@@ -14,217 +14,258 @@ import { UserContext } from '../../contextapi/UserContext';
 import { UserProfileDetailsContext } from '../../contextapi/UserProfileDetailsContext';
 import style from './Profile.module.scss';
 import ModalBox from '../../components/Modal/ModalBox';
-import { UpdateUserProfileProps, updateSingleUserProfile, getLogedInUserProfile } from '../../services/API';
+import {
+	UpdateUserProfileProps,
+	updateSingleUserProfile,
+	getLogedInUserProfile,
+} from '../../services/API';
 import { useUserContext } from '../../contextapi/UserContextCookies';
 import { loginSuccess } from '../../redux/userSlice';
 
 const Profile = () => {
-  // to use redux toolkit
-  const userProfileDetails = useSelector((state: any) => state.user.currentUser);
-  const dispatch = useDispatch();
+	// to use redux toolkit
+	const userProfileDetails = useSelector(
+		(state: any) => state.user.currentUser
+	);
+	const dispatch = useDispatch();
 
-  ////////////////////////////////////////////////////////////////////////////////
-  //Context api state - these are context api that i used previously.
-  // Now will not use this context api
-  // const [state, setState] = useContext(UserContext);
-  // const {userProfileDetails,updateUserProfileDetails} = useContext(UserProfileDetailsContext);
-  // Context API to update new user info -Cookies context api
-  const { user, setUser } = useUserContext();
-  ////////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////////
+	//Context api state - these are context api that i used previously.
+	// Now will not use this context api
+	// const [state, setState] = useContext(UserContext);
+	// const {userProfileDetails,updateUserProfileDetails} = useContext(UserProfileDetailsContext);
+	// Context API to update new user info -Cookies context api
+	const { user, setUser } = useUserContext();
+	////////////////////////////////////////////////////////////////////////////////
 
-  /****************************************/
-  /******  To Open Modal Box     **********/
-  /****************************************/
-  const [open, setOpen] = useState<boolean>(false);
-  const onOpenModal = () => {
-    setOpen(true);
-  };
-  const onCloseModal = () => {
-    setOpen(false);
-  };
+	/****************************************/
+	/******  To Open Modal Box     **********/
+	/****************************************/
+	const [open, setOpen] = useState<boolean>(false);
+	const onOpenModal = () => {
+		setOpen(true);
+	};
+	const onCloseModal = () => {
+		setOpen(false);
+	};
 
-  /****************************************/
-  /******  To Update User Profile    ******/
-  /****************************************/
+	/****************************************/
+	/******  To Update User Profile    ******/
+	/****************************************/
 
-  const [name, setName] = useState<string>('');
-  const [email, setEmail] = useState<string>('');
-  const [profilePic, setProfilePic] = useState<string>('');
-  const [userSkills, setUserSkills] = useState<string[]>([]);
-  const [addSkills, setAddSkills] = useState<string>('');
+	const [name, setName] = useState<string>('');
+	const [email, setEmail] = useState<string>('');
+	const [profilePic, setProfilePic] = useState<string>('');
+	const [userSkills, setUserSkills] = useState<string[]>([]);
+	const [addSkills, setAddSkills] = useState<string>('');
 
-  const onSubmitUpdateProfile = async () => {
-    try {
-      const payload: UpdateUserProfileProps = {
-        name: name,
-        email: email,
-        imageUrl: profilePic,
-        skills: userSkills,
-      };
-      const res = await updateSingleUserProfile(userProfileDetails?._id!, payload);
-      if (res) {
-        toast.success('Successfully Updated Profile', {
-          position: toast.POSITION.TOP_CENTER,
-        });
+	const onSubmitUpdateProfile = async () => {
+		try {
+			const payload: UpdateUserProfileProps = {
+				name: name,
+				email: email,
+				imageUrl: profilePic,
+				skills: userSkills,
+			};
+			const res = await updateSingleUserProfile(
+				userProfileDetails?._id!,
+				payload
+			);
+			if (res) {
+				toast.success('Successfully Updated Profile', {
+					position: toast.POSITION.TOP_CENTER,
+				});
 
-        dispatch(loginSuccess(res.user));
+				dispatch(loginSuccess(res.user));
 
-        /////////////////////////////////////////
-        // To update the user context api with the updated profile data need to set response in the local storage
-        // Then also need to update the setState so that newly profile info can be added in th state and can show in the
-        // profile page
-        // localStorage.setItem("tokenLogin", JSON.stringify(res));
-        // setState({
-        //   user: res.user,
-        // });
-        ////////////////////////////////////////////
-        // to update user profile in the context
-        // updateUserProfileDetails(res.user);
-        // When user update their profile we need to update user details in the context api with this function
-        // updateUserProfileDetails(res?.user!);
-        ////////////////////////////////////////////
-        // From cookie context api - to update user info in the context api as soon as user update user info
-        setUser(res.user);
-        /////////////////////////////////////////
-      }
-    } catch (error: any) {
-      toast.error(error.response && error.response.data.error, {
-        position: toast.POSITION.TOP_RIGHT,
-      });
-    }
-  };
+				/////////////////////////////////////////
+				// To update the user context api with the updated profile data need to set response in the local storage
+				// Then also need to update the setState so that newly profile info can be added in th state and can show in the
+				// profile page
+				// localStorage.setItem("tokenLogin", JSON.stringify(res));
+				// setState({
+				//   user: res.user,
+				// });
+				////////////////////////////////////////////
+				// to update user profile in the context
+				// updateUserProfileDetails(res.user);
+				// When user update their profile we need to update user details in the context api with this function
+				// updateUserProfileDetails(res?.user!);
+				////////////////////////////////////////////
+				// From cookie context api - to update user info in the context api as soon as user update user info
+				setUser(res.user);
+				/////////////////////////////////////////
+			}
+		} catch (error: any) {
+			toast.error(error.response && error.response.data.error, {
+				position: toast.POSITION.TOP_RIGHT,
+			});
+		}
+	};
 
-  const addUserSkills = () => {
-    setUserSkills([...userSkills, addSkills]);
-    setAddSkills('');
-  };
+	const addUserSkills = () => {
+		setUserSkills([...userSkills, addSkills]);
+		setAddSkills('');
+	};
 
-  const removeSkills = (skillIndex: string) => {
-    const skills = userSkills.filter((item) => item !== skillIndex);
-    setUserSkills(skills);
-  };
+	const removeSkills = (skillIndex: string) => {
+		const skills = userSkills.filter((item) => item !== skillIndex);
+		setUserSkills(skills);
+	};
 
-  useEffect(() => {
-    setName(userProfileDetails?.name!);
-    setEmail(userProfileDetails?.email!);
-    setProfilePic(userProfileDetails?.imageUrl!);
-    setUserSkills(userProfileDetails?.skills);
-  }, []);
+	useEffect(() => {
+		setName(userProfileDetails?.name!);
+		setEmail(userProfileDetails?.email!);
+		setProfilePic(userProfileDetails?.imageUrl!);
+		setUserSkills(userProfileDetails?.skills);
+	}, []);
 
-  /////////////////////////////////////////////
-  // With context api
-  // useEffect(() => {
-  //   setName(user?.name!);
-  //   setEmail(user?.email!);
-  //   setProfilePic(user?.imageUrl!);
-  // }, [user]);
-  /////////////////////////////////////////////
+	/////////////////////////////////////////////
+	// With context api
+	// useEffect(() => {
+	//   setName(user?.name!);
+	//   setEmail(user?.email!);
+	//   setProfilePic(user?.imageUrl!);
+	// }, [user]);
+	/////////////////////////////////////////////
 
-  /****************************************/
-  /******  To Show User Profile    ********/
-  /****************************************/
-  // This code will load user profile info- but its only for the test purpose
-  // const [userProfileDetails, setUserInfo] = useState<UserProfileDetails>();
-  // const loadUserProfileDetails = async () => {
-  //   try {
-  //     const res = await getLogedInUserProfile();
-  //     if (res) {
-  //       setUserInfo(res);
-  //     }
-  //   } catch (error: any) {
-  //     toast.error(error.response && error.response.data.error, {
-  //       position: toast.POSITION.TOP_RIGHT,
-  //     });
-  //   }
-  // };
-  // useEffect(() => {
-  //   loadUserProfileDetails();
-  // }, []);
+	/****************************************/
+	/******  To Show User Profile    ********/
+	/****************************************/
+	// This code will load user profile info- but its only for the test purpose
+	// const [userProfileDetails, setUserInfo] = useState<UserProfileDetails>();
+	// const loadUserProfileDetails = async () => {
+	//   try {
+	//     const res = await getLogedInUserProfile();
+	//     if (res) {
+	//       setUserInfo(res);
+	//     }
+	//   } catch (error: any) {
+	//     toast.error(error.response && error.response.data.error, {
+	//       position: toast.POSITION.TOP_RIGHT,
+	//     });
+	//   }
+	// };
+	// useEffect(() => {
+	//   loadUserProfileDetails();
+	// }, []);
 
-  return (
-    <SubscriberPageLayout>
-      <CardLayout title="Account Details">
-        <div className={style.profileContainer}>
-          {/*  To show profile picture and if user did not add any profile picture then an avatar will be shown here */}
-          <div>
-            {user?.name}
-            {userProfileDetails?.imageUrl ? (
-              <div className={style.profilePicture}>
-                <img src={userProfileDetails?.imageUrl} />
-              </div>
-            ) : (
-              <div className={style.profilePictureAvatar}>
-                <p className={style.profilePictureIcon}>
-                  <CgProfile size={80} />
-                </p>
-              </div>
-            )}
-            {userProfileDetails?.award.map((award: any, index: number) => (
-              <p key={index}>{award}</p>
-            ))}
+	return (
+		<SubscriberPageLayout>
+			<CardLayout title='Account Details'>
+				<div className={style.profileContainer}>
+					{/*  To show profile picture and if user did not add any profile picture then an avatar will be shown here */}
+					<div>
+						{user?.name}
+						{userProfileDetails?.imageUrl ? (
+							<div className={style.profilePicture}>
+								<img src={userProfileDetails?.imageUrl} alt='profileimg' />
+							</div>
+						) : (
+							<div className={style.profilePictureAvatar}>
+								<p className={style.profilePictureIcon}>
+									<CgProfile size={80} />
+								</p>
+							</div>
+						)}
+						{userProfileDetails?.award?.map((award: any, index: number) => (
+							<p key={index}>{award}</p>
+						))}
 
-            <div className={style.skillsDesing}>
-              {userProfileDetails?.skills.map((skill: any, index: number) => (
-                <p key={index}>{skill}</p>
-              ))}
-            </div>
-          </div>
+						<div className={style.skillsDesing}>
+							{userProfileDetails?.skills?.map((skill: any, index: number) => (
+								<p key={index}>{skill}</p>
+							))}
+						</div>
+					</div>
 
-          <div className={style.profileDetails}>
-            <h6>Id: {userProfileDetails?._id}</h6>
-            <h6>Name: {userProfileDetails?.name}</h6>
-            <h6>E-mail: {userProfileDetails?.email}</h6>
-            <h6>Role: {userProfileDetails?.role}</h6>
-            <h6>Joined: {userProfileDetails?.date}</h6>
-            <h6>Points: {userProfileDetails?.points}</h6>
-          </div>
+					<div className={style.profileDetails}>
+						<h6>Id: {userProfileDetails?._id}</h6>
+						<h6>Name: {userProfileDetails?.name}</h6>
+						<h6>E-mail: {userProfileDetails?.email}</h6>
+						<h6>Role: {userProfileDetails?.role}</h6>
+						<h6>Joined: {userProfileDetails?.date}</h6>
+						<h6>Points: {userProfileDetails?.points}</h6>
+					</div>
 
-          <div className={style.editIcon} onClick={onOpenModal}>
-            <CiEdit size={35} color="green" />
-          </div>
-        </div>
-      </CardLayout>
+					<div className={style.editIcon} onClick={onOpenModal}>
+						<CiEdit size={35} color='green' />
+					</div>
+				</div>
+			</CardLayout>
 
-      {/* //////////////////////////////////////////////////////////////////////// */}
-      {/* ////             Modal Box to Update User Profile                /////// */}
-      {/* //////////////////////////////////////////////////////////////////////// */}
+			{/* //////////////////////////////////////////////////////////////////////// */}
+			{/* ////             Modal Box to Update User Profile                /////// */}
+			{/* //////////////////////////////////////////////////////////////////////// */}
 
-      {/* To update user info - Modal Box */}
+			{/* To update user info - Modal Box */}
 
-      <ModalBox open={open} onCloseModal={onCloseModal} onSaveButton={onSubmitUpdateProfile} title="Update Profile">
-        <label>Name:</label>
-        <div className="form-group">
-          <input type="text" name="Name" className={style.userForm} value={name} onChange={(e) => setName(e.target.value)} />
-        </div>
-        <label>E-mail:</label>
-        <div className="form-group">
-          <input type="text" name="Name" className={style.userForm} value={email} onChange={(e) => setEmail(e.target.value)} />
-        </div>
-        <label>Profile Pic:</label>
-        <div className="form-group">
-          <input type="text" name="Name" className={style.userForm} value={profilePic} onChange={(e) => setProfilePic(e.target.value)} />
-        </div>
-        <div className="form-group">
-          <input type="text" name="Name" className={style.userForm} value={addSkills} onChange={(e) => setAddSkills(e.target.value)} />
-        </div>
+			<ModalBox
+				open={open}
+				onCloseModal={onCloseModal}
+				onSaveButton={onSubmitUpdateProfile}
+				title='Update Profile'
+			>
+				<label>Name:</label>
+				<div className='form-group'>
+					<input
+						type='text'
+						name='Name'
+						className={style.userForm}
+						value={name}
+						onChange={(e) => setName(e.target.value)}
+					/>
+				</div>
+				<label>E-mail:</label>
+				<div className='form-group'>
+					<input
+						type='text'
+						name='Name'
+						className={style.userForm}
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+					/>
+				</div>
+				<label>Profile Pic:</label>
+				<div className='form-group'>
+					<input
+						type='text'
+						name='Name'
+						className={style.userForm}
+						value={profilePic}
+						onChange={(e) => setProfilePic(e.target.value)}
+					/>
+				</div>
+				<div className='form-group'>
+					<input
+						type='text'
+						name='Name'
+						className={style.userForm}
+						value={addSkills}
+						onChange={(e) => setAddSkills(e.target.value)}
+					/>
+				</div>
 
-        <div className={style.skills_container}>
-          {userSkills.map((skill, index) => (
-            <div key={index} className={style.skill_item}>
-              <p className={style.skill}>{skill}</p>
-              <p className={style.close_icon} onClick={() => removeSkills(skill)}>
-                <IoMdClose />
-              </p>
-            </div>
-          ))}
-        </div>
+				<div className={style.skills_container}>
+					{userSkills.map((skill, index) => (
+						<div key={index} className={style.skill_item}>
+							<p className={style.skill}>{skill}</p>
+							<p
+								className={style.close_icon}
+								onClick={() => removeSkills(skill)}
+							>
+								<IoMdClose />
+							</p>
+						</div>
+					))}
+				</div>
 
-        <button className="btn btn-warning" onClick={addUserSkills}>
-          Add Skills
-        </button>
-      </ModalBox>
-    </SubscriberPageLayout>
-  );
+				<button className='btn btn-warning' onClick={addUserSkills}>
+					Add Skills
+				</button>
+			</ModalBox>
+		</SubscriberPageLayout>
+	);
 };
 
 export default Profile;
