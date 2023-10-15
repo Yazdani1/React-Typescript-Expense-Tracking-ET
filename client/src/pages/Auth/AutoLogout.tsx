e13acfe29c78f158e8498b98f62ca4548093787e
yazdani1 <yaz4dani@gmail.com>
2023-10-15 23:30:47 +0200

    WIP

--- client/src/pages/Auth/AutoLogout.tsx
+++ client/src/pages/Auth/AutoLogout.tsx
@@ -6,41 +6,42 @@ import { useIdleTimer } from 'react-idle-timer';
 import { ToastContainer, toast } from 'react-toastify';
 
 const AutoLogout: React.FC = () => {
-  const location = useLocation();
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-  const tokenData = localStorage.getItem('token');
-  const lastVisitedRoute = localStorage.getItem('lastVisitedRoute');
+	const location = useLocation();
+	const navigate = useNavigate();
+	const dispatch = useDispatch();
+	const tokenData = localStorage.getItem('token');
+	const lastVisitedRoute = localStorage.getItem('lastVisitedRoute');
 
-  const handleGetTestData = () => {
-    toast.error('You are not active', {
-      position: toast.POSITION.TOP_CENTER,
-    });
-  };
+	const handleGetTestData = () => {
+		toast.error('You are not active', {
+			position: toast.POSITION.TOP_CENTER,
+		});
+	};
 
-  const onIdle = () => {
-    if (tokenData) {
-      handleGetTestData();
-      dispatch(logOut());
-      window.localStorage.removeItem('tokenLogin');
-      window.localStorage.removeItem('token');
-      window.localStorage.removeItem('userInforProtectedRoute');
-      window.localStorage.removeItem('userData');
+	const onIdle = () => {
+		if (tokenData) {
+			handleGetTestData();
+			dispatch(logOut());
+			window.localStorage.removeItem('tokenLogin');
+			window.localStorage.removeItem('token');
+			window.localStorage.removeItem('userInforProtectedRoute');
+			window.localStorage.removeItem('userData');
 
-      // Store the current route as the last visited route when user auto logout
-      localStorage.setItem('lastVisitedRoute', location.pathname);
+			// Store the current route as the last visited route when user auto logout
 
-      navigate('/signin');
-    }
-  };
+			localStorage.setItem('lastVisitedRoute', location.pathname);
 
-  useIdleTimer({
-    onIdle,
-    timeout: 10_000, // Set the timeout to 10 seconds
-    throttle: 500,
-  });
+			navigate('/signin');
+		}
+	};
 
-  return null;
+	useIdleTimer({
+		onIdle,
+		timeout: 10_000, // Set the timeout to 10 seconds
+		throttle: 500,
+	});
+
+	return null;
 };
 
 export default AutoLogout;
--- client/src/pages/ExpenseBook/ExpenseBookCard.tsx
+++ client/src/pages/ExpenseBook/ExpenseBookCard.tsx
@@ -1,37 +1,38 @@
-import { FC } from "react";
-import moment from "moment";
-import { Link } from "react-router-dom";
+import { FC } from 'react';
+import moment from 'moment';
+import { Link } from 'react-router-dom';
 
-import expenseBookCardStyle from "./ExpenseBookCard.module.scss";
-import { ExpenseBookInfo, ExpenseBookColor } from "../../services/DataProvider";
+import expenseBookCardStyle from './ExpenseBookCard.module.scss';
+import { ExpenseBookInfo, ExpenseBookColor } from '../../services/DataProvider';
 
 interface ExpenseBookCardProps {
-  expense_book: ExpenseBookInfo;
+	expense_book: ExpenseBookInfo;
 }
 
 const ExpenseBookCard: FC<ExpenseBookCardProps> = ({ expense_book }) => {
-  return (
-    <div
-      className={
-        ExpenseBookColor.Orange === expense_book.color
-          ? expenseBookCardStyle.expenseBookCardOrange
-          : ExpenseBookColor.Yellow === expense_book.color
-          ? expenseBookCardStyle.expenseBookCardYellow
-          : expenseBookCardStyle.expenseBookCardGreen
-      }
-    >
-      <Link
-        to={"/expense-details/" + expense_book.slug}
-        style={{ textDecoration: "none", color: "inherit" }}
-      >
-        <div className={expenseBookCardStyle.itemPosition}>
-          <h6>{expense_book.name.substring(0, 15)}</h6>
-          <p>{expense_book.color}</p>
-          <p>{moment(expense_book?.date).format("MMM Do YYYY")}</p>
-        </div>
-      </Link>
-    </div>
-  );
+	return (
+		<div
+			className={
+				ExpenseBookColor.Orange === expense_book.color
+					? expenseBookCardStyle.expenseBookCardOrange
+					: ExpenseBookColor.Yellow === expense_book.color
+					? expenseBookCardStyle.expenseBookCardYellow
+					: expenseBookCardStyle.expenseBookCardGreen
+			}
+		>
+			<Link
+				to={'/expense-details/' + expense_book.slug}
+				style={{ textDecoration: 'none', color: 'inherit' }}
+			>
+				<div className={expenseBookCardStyle.itemPosition}>
+					<h6>{expense_book.name.substring(0, 15)}</h6>
+
+					<p>{expense_book.color}</p>
+					<p>{moment(expense_book?.date).format('MMM Do YYYY')}</p>
+				</div>
+			</Link>
+		</div>
+	);
 };
 
 export default ExpenseBookCard;
