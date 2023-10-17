a56f63794c38cb8111d04687e8bd35a5176a081b
yazdani1 <yaz4dani@gmail.com>
2023-10-17 21:52:00 +0200

    WIP

--- client/src/pages/Courses/Courses.tsx
+++ client/src/pages/Courses/Courses.tsx
@@ -1,7 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
 
-import style from './Course.module.scss';
 import SubscriberPageLayout from '../../layouts/SubscriberPageLayout';
 import { getCourseListsForSubscriber } from '../../services/API';
 import { Course } from '../../services/DataProvider';
@@ -9,44 +8,44 @@ import CardLayout from '../../components/CardLayout/CardLayout';
 import CourseCard from './CourseCard';
 
 const Courses = () => {
-  /****************************************/
-  /*********** Course Lists ***************/
-  /****************************************/
+	/****************************************/
+	/*********** Course Lists ***************/
+	/****************************************/
 
-  const [courseLists, setCourseLists] = useState<Course[]>([]);
+	const [courseLists, setCourseLists] = useState<Course[]>([]);
 
-  const loadAllCourses = async () => {
-    try {
-      const res = await getCourseListsForSubscriber();
+	const loadAllCourses = async () => {
+		try {
+			const res = await getCourseListsForSubscriber();
 
-      if (res) {
-        setCourseLists(res);
-      }
-    } catch (error: any) {
-      toast.error(error.response && error.response.data.error, {
-        position: toast.POSITION.TOP_CENTER,
-      });
-    }
-  };
+			if (res) {
+				setCourseLists(res);
+			}
+		} catch (error: any) {
+			toast.error(error.response && error.response.data.error, {
+				position: toast.POSITION.TOP_CENTER,
+			});
+		}
+	};
 
-  useEffect(() => {
-    loadAllCourses();
-  }, []);
+	useEffect(() => {
+		loadAllCourses();
+	}, []);
 
-  return (
-    <SubscriberPageLayout>
-      <CardLayout>
-        <div className="row">
-          {courseLists &&
-            courseLists.map((course) => (
-              <div className="col-xl-4 col-lg-4">
-                <CourseCard course={course} />
-              </div>
-            ))}
-        </div>
-      </CardLayout>
-    </SubscriberPageLayout>
-  );
+	return (
+		<SubscriberPageLayout>
+			<CardLayout>
+				<div className='row'>
+					{courseLists &&
+						courseLists.map((course) => (
+							<div className='col-xl-4 col-lg-4'>
+								<CourseCard course={course} />
+							</div>
+						))}
+				</div>
+			</CardLayout>
+		</SubscriberPageLayout>
+	);
 };
 
 export default Courses;
