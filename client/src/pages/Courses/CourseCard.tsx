7c1842f2f3de7f458da0a8a656fb88aff23045f8
yazdani1 <yaz4dani@gmail.com>
2023-10-20 23:48:55 +0200

    WIP

--- client/src/pages/Courses/CourseCard.tsx
+++ client/src/pages/Courses/CourseCard.tsx
@@ -5,27 +5,41 @@ import { Course } from '../../services/DataProvider';
 import style from './Course.module.scss';
 
 interface CourseCardProps {
-  course: Course;
+	course: Course;
 }
 
 const CourseCard: FC<CourseCardProps> = ({ course }) => {
-  return (
-    <Link to={'/course-details-page/' + course.slug} style={{ textDecoration: 'none', color: 'inherit' }}>
-      <div className={style.courseContainer}>
-        <h6>{course.title}</h6>
-        <p>{course.postedBy?.name}</p>
-        <hr />
-        <p>{course.des.substring(0, 50)}</p>
-        <div className={style.itemRow}>
-          <p>Enrolled:{course.enrolledStudents}</p>
-          <p>Seats:{course.maxStudents}</p>
-          {course.enrolledStudents === course.maxStudents && (
-            <p style={{ background: 'red', padding: '5px', color: 'white', borderRadius: '20px' }}>No seats!</p>
-          )}
-        </div>
-      </div>
-    </Link>
-  );
+	return (
+		<Link
+			to={'/course-details-page/' + course.slug}
+			style={{ textDecoration: 'none', color: 'inherit' }}
+		>
+			<div className={style.courseContainer}>
+				<h6>{course.title}</h6>
+				<p>{course.postedBy?.name}</p>
+
+				<hr />
+				<p>{course.des.substring(0, 50)}</p>
+
+				<div className={style.itemRow}>
+					<p>Enrolled:{course.enrolledStudents}</p>
+					<p>Seats:{course.maxStudents}</p>
+					{course.enrolledStudents === course.maxStudents && (
+						<p
+							style={{
+								background: 'red',
+								padding: '5px',
+								color: 'white',
+								borderRadius: '20px',
+							}}
+						>
+							No seats!
+						</p>
+					)}
+				</div>
+			</div>
+		</Link>
+	);
 };
 
 export default CourseCard;
