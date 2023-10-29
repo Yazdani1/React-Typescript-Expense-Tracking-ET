e023a3a2195fbb828d1f13df2418a54667ba328a
yazdani1 <yaz4dani@gmail.com>
2023-10-29 23:55:42 +0100

    WIP

--- client/src/pages/Courses/Courses.tsx
+++ client/src/pages/Courses/Courses.tsx
@@ -11,7 +11,6 @@ const Courses = () => {
 	/****************************************/
 	/*********** Course Lists ***************/
 	/****************************************/
-
 	const [courseLists, setCourseLists] = useState<Course[]>([]);
 
 	const loadAllCourses = async () => {
