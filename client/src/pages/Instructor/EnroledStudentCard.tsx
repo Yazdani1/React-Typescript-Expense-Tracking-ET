56027f2f4e3f1354feda4d3c4e8f80674fbc8dad
yazdani1 <yaz4dani@gmail.com>
2023-11-02 23:34:36 +0100

    WIP

--- client/src/pages/Instructor/EnroledStudentCard.tsx
+++ client/src/pages/Instructor/EnroledStudentCard.tsx
@@ -1,23 +1,22 @@
 import React, { FC } from 'react';
-import { Link } from 'react-router-dom';
 
 import { CourseEnrolmentItems } from '../../services/DataProvider';
-import style from './InstructorDashboard.module.scss';
+// import style from './InstructorDashboard.module.scss';
 import CardLayout from '../../components/CardLayout/CardLayout';
 
 interface EnroledStudentCardProps {
-  student: CourseEnrolmentItems;
+	student: CourseEnrolmentItems;
 }
 
 const EnroledStudentCard: FC<EnroledStudentCardProps> = ({ student }) => {
-  return (
-    <CardLayout>
-      <h6>{student.enrolledBy?.name}</h6>
-      <p>{student.enrolledBy?.email}</p>
-      <p>{student.coupon}</p>
-      <p>{student.date}</p>
-    </CardLayout>
-  );
+	return (
+		<CardLayout>
+			<h6>{student.enrolledBy?.name}</h6>
+			<p>{student.enrolledBy?.email}</p>
+			<p>{student.coupon}</p>
+			<p>{student.date}</p>
+		</CardLayout>
+	);
 };
 
 export default EnroledStudentCard;
