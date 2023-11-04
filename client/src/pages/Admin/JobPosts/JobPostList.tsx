115d7d4a21f600ba1e980f2cd3df964defd42826
yazdani1 <yaz4dani@gmail.com>
2023-11-04 22:12:25 +0100

    WIP

--- client/src/pages/Admin/JobPosts/JobPostList.tsx
+++ client/src/pages/Admin/JobPosts/JobPostList.tsx
@@ -8,50 +8,56 @@ import { JobPosts } from '../../../services/DataProvider';
 import JobPostsCard from './JobPostsCard';
 
 const JobPostList = () => {
-  /****************************************/
-  /******All job posts lists  *************/
-  /****************************************/
+	/****************************************/
+	/******All job posts lists  *************/
+	/****************************************/
 
-  const [allJobPosts, setAllJobPosts] = useState<JobPosts[]>([]);
+	const [allJobPosts, setAllJobPosts] = useState<JobPosts[]>([]);
 
-  const loadAllEmployerJobPosts = async () => {
-    try {
-      const res = await getAllEmployerJobPosts();
-      if (res) {
-        setAllJobPosts(res);
-      }
-    } catch (error: any) {
-      toast.error(error.response && error.response.data.error, {
-        position: toast.POSITION.BOTTOM_CENTER,
-      });
-    }
-  };
+	const loadAllEmployerJobPosts = async () => {
+		try {
+			const res = await getAllEmployerJobPosts();
+			if (res) {
+				setAllJobPosts(res);
+			}
+		} catch (error: any) {
+			toast.error(error.response && error.response.data.error, {
+				position: toast.POSITION.BOTTOM_CENTER,
+			});
+		}
+	};
 
-  useEffect(() => {
-    loadAllEmployerJobPosts();
-  }, []);
-
-  return (
-    <AdminPageLayout>
-      <JobPostItemRow />
-      {allJobPosts && allJobPosts.map((job) => <JobPostsCard jobpost={job} loadJobposts={loadAllEmployerJobPosts} key={job._id} />)}
-    </AdminPageLayout>
-  );
+	useEffect(() => {
+		loadAllEmployerJobPosts();
+	}, []);
+	return (
+		<AdminPageLayout>
+			<JobPostItemRow />
+			{allJobPosts &&
+				allJobPosts.map((job) => (
+					<JobPostsCard
+						jobpost={job}
+						loadJobposts={loadAllEmployerJobPosts}
+						key={job._id}
+					/>
+				))}
+		</AdminPageLayout>
+	);
 };
 
 export default JobPostList;
 
 const JobPostItemRow = () => {
-  return (
-    <div className={style.jobpost_header_row}>
-      <h6>Title</h6>
-      <p>Visibility</p>
-      <p>Status</p>
-      <p>City</p>
-      <p>Application</p>
-      <p>Published By</p>
-      <p>Edit</p>
-      <p>Preview</p>
-    </div>
-  );
+	return (
+		<div className={style.jobpost_header_row}>
+			<h6>Title</h6>
+			<p>Visibility</p>
+			<p>Status</p>
+			<p>City</p>
+			<p>Application</p>
+			<p>Published By</p>
+			<p>Edit</p>
+			<p>Preview</p>
+		</div>
+	);
 };
