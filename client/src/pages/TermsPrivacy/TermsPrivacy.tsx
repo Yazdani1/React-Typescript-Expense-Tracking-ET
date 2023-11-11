f68298bfe2616a02e9ca0772e199237a5f47f85e
yazdani1 <yaz4dani@gmail.com>
2023-11-11 22:24:51 +0100

    WIP

--- client/src/pages/TermsPrivacy/TermsPrivacy.tsx
+++ client/src/pages/TermsPrivacy/TermsPrivacy.tsx
@@ -1,28 +1,42 @@
 import React, { useEffect, useState } from 'react';
 import data from './t.json';
 const TermsPrivacy = () => {
+	const renderSubSection = (subSection: any, level: number) => (
+		<div key={subSection.type} style={{ marginLeft: `${level * 20}px` }}>
+			<p>{`Type: ${subSection.type}, Content: ${subSection.content}`}</p>
+		</div>
+	);
+
+	const renderSection = (section: any, level: number) => (
+		<div key={section.type} style={{ marginLeft: `${level * 20}px` }}>
+			<h3>{section.content}</h3>
+			{section.details && <p>{section.details}</p>}
+			{section.subSections && (
+				<div>
+					{section.subSections.map((subSection: any, subIndex: number) => (
+						<div key={subIndex}>{renderSubSection(subSection, level + 1)}</div>
+					))}
+				</div>
+			)}
+		</div>
+	);
+
 	return (
 		<div>
-			<h1>Data Component</h1>
-			<p
-				style={{ marginLeft: '10px' }}
-				dangerouslySetInnerHTML={{ __html: data.data.replace(/\n/g, '<br />') }}
-			></p>
-			{/* {data.map((item, index) => (
-				<div style={{ padding: '20px' }}>
-					<h6>{item.title}</h6>
-					<p
-						dangerouslySetInnerHTML={{
-							__html: item.data.replace(/\n/g, '<br />'),
-						}}
-					></p>
-				</div>
-			))} */}
+			<h1>{data.title}</h1>
+			{data.sections.map((section: any, index: number) => (
+				<div key={index}>{renderSection(section, index + 1)}</div>
+			))}
 		</div>
 	);
 };
 
 export default TermsPrivacy;
+
+// {
+// 	"title": "Test title is here doing so many things that need to be done",
+// 	"data": "1. Text data is here,\n\nThis is just a description\n\n2. Next generation:\n   1. First day work:\n     We are looking for first-day work, not for the last time of the month. This is the main reason here to solve the issues.\n   2. We are all here for different reasons.\n      (a) Test a for title two\n      (b) Test b for the new one"
+// }
 
 // [
 // 	{
