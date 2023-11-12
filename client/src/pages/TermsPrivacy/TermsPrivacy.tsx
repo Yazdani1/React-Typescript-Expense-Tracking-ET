4ae45827d9b14ba93f1fde0fbc47ecbe0b162b1a
yazdani1 <yaz4dani@gmail.com>
2023-11-12 23:34:36 +0100

    WIP

--- client/src/pages/TermsPrivacy/TermsPrivacy.tsx
+++ client/src/pages/TermsPrivacy/TermsPrivacy.tsx
@@ -35,14 +35,14 @@ export default TermsPrivacy;
 // 	"title": "Test title is here doing so many things that need to be done",
 // 	"data": "1. Text data is here,\n\nThis is just a description\n\n2. Next generation:\n   1. First day work:\n     We are looking for first-day work, not for the last time of the month. This is the main reason here to solve the issues.\n   2. We are all here for different reasons.\n      (a) Test a for title two\n      (b) Test b for the new one"
 // }
-
 // [
 // 	{
 // 		title: 'Test title',
 // 		data: '1. Text data is here, This is just a description\n2. Next generation:\n   1. First day work:\n     We are looking for first day work not for the last time of the month. This is the main reason here to solve the issues.\n   2. We all are here for different reason.',
 // 	},
+
 // 	{
-// 		title: 'Test title two',
+//	title: 'Test title two',
 // 		data: '1. Text data is here, This is just a description\n2. Next generation:\n   1. First day work:\n     We are looking for first day work not for the last time of the month. This is the main reason here to solve the issues.\n   2. We all are here for different reason.',
 // 	},
 // ];
