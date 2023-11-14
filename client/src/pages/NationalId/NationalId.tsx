f2a2b37791f9694a057ba0ad4ccb5daf8f25b984
yazdani1 <yaz4dani@gmail.com>
2023-11-14 20:12:31 +0100

    WIP

--- client/src/pages/NationalId/NationalId.tsx
+++ client/src/pages/NationalId/NationalId.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { toast } from 'react-toastify';
 
 import CardLayout from '../../components/CardLayout/CardLayout';
@@ -9,124 +9,124 @@ import { searchNationalId } from '../../services/API';
 import { useIncomeRecordContext } from '../../contextapi/IncomeRecordContext';
 
 const NationalId = () => {
-  const { allIncomeRecords } = useIncomeRecordContext();
+	const { allIncomeRecords } = useIncomeRecordContext();
 
-  // To add search value from the input fields
-  const [nationaId, setNationalId] = useState<number | any>('');
-  const [error, setError] = useState<string>();
+	// To add search value from the input fields
+	const [nationaId, setNationalId] = useState<number | any>('');
+	const [error, setError] = useState<string>();
 
-  // To store national id search result data
-  const [nationalIdDetails, setNationalIdDetails] = useState<NationalID | null>();
+	// To store national id search result data
+	const [nationalIdDetails, setNationalIdDetails] =
+		useState<NationalID | null>();
 
-  const onSubmitSearchNationalId = async () => {
-    try {
-      // To replace the space from the nationaId state to pass it in the api url to search the data
-      const nationalIdWithoutSpaces = nationaId.replace(/\s/g, ''); // Remove spaces
-      const res = await searchNationalId(nationalIdWithoutSpaces!);
-      if (res) {
-        setNationalIdDetails(res);
-        // To clean the error state if we get the response
-        setError('');
-      }
-    } catch (error: any) {
-      setError(error.response && error.response.data.error);
-      // if it retunrs an error for wrong id then it should empty the state
-      setNationalIdDetails(null);
+	const onSubmitSearchNationalId = async () => {
+		try {
+			// To replace the space from the nationaId state to pass it in the api url to search the data
+			const nationalIdWithoutSpaces = nationaId.replace(/\s/g, ''); // Remove spaces
+			const res = await searchNationalId(nationalIdWithoutSpaces!);
+			if (res) {
+				setNationalIdDetails(res);
+				// To clean the error state if we get the response
+				setError('');
+			}
+		} catch (error: any) {
+			setError(error.response && error.response.data.error);
+			// if it retunrs an error for wrong id then it should empty the state
+			setNationalIdDetails(null);
 
-      toast.error(error.response && error.response.data.error, {
-        position: toast.POSITION.TOP_RIGHT,
-      });
-    }
-  };
-  // This function code is used to have a space after each 4 digit in the search input field
-  // It will take total 13 digit and one space after each 4 digit.
-  // Then it's search query will load the data through query from api url
+			toast.error(error.response && error.response.data.error, {
+				position: toast.POSITION.TOP_RIGHT,
+			});
+		}
+	};
+	// This function code is used to have a space after each 4 digit in the search input field
+	// It will take total 13 digit and one space after each 4 digit.
+	// Then it's search query will load the data through query from api url
 
-  const handleNationalIdChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const input = e.target.value;
-    const sanitizedInput = input.replace(/\D/g, ''); // Remove non-digit characters
-    let formattedInput = sanitizedInput.substr(0, 16); // Take the first 13 digits
-    if (formattedInput.length > 4) {
-      // Insert spaces after every 4 digits
-      formattedInput = formattedInput.replace(/(\d{4})/g, '$1 ');
-    }
-    if (formattedInput.length <= 16) {
-      //This is the state where it load theinput
-      setNationalId(formattedInput);
-    }
-  };
+	const handleNationalIdChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const input = e.target.value;
+		const sanitizedInput = input.replace(/\D/g, ''); // Remove non-digit characters
+		let formattedInput = sanitizedInput.substr(0, 16); // Take the first 13 digits
+		if (formattedInput.length > 4) {
+			// Insert spaces after every 4 digits
+			formattedInput = formattedInput.replace(/(\d{4})/g, '$1 ');
+		}
+		if (formattedInput.length <= 16) {
+			//This is the state where it load theinput
+			setNationalId(formattedInput);
+		}
+	};
 
-  return (
-    <HomePageLayout>
-      <div className="container">
-        <div className="row">
-          <div className="col-xl-4">
-            <CardLayout>
-              <h5>{allIncomeRecords.length}</h5>
-            </CardLayout>
-            <CardLayout>
-              <h3>Nationa Id Details You Can Search Here</h3>
-            </CardLayout>
-          </div>
+	return (
+		<HomePageLayout>
+			<div className='container'>
+				<div className='row'>
+					<div className='col-xl-4'>
+						<CardLayout>
+							<h5>{allIncomeRecords.length}</h5>
+						</CardLayout>
+						<CardLayout>
+							<h3>Nationa Id Details You Can Search Here</h3>
+						</CardLayout>
+					</div>
 
-          <div className="col-xl-8">
-            <CardLayout>
-              <div className={style.search_nationaid}>
-                <div className="form-group">
-                  <input
-                    type="text" // Use type "text" instead of "number" to allow non-digit characters
-                    name="Name"
-                    className={style.searchInputForm}
-                    placeholder="your national id number *"
-                    value={nationaId}
-                    onChange={handleNationalIdChange}
-                  />
-                </div>
+					<div className='col-xl-8'>
+						<CardLayout>
+							<div className={style.search_nationaid}>
+								<div className='form-group'>
+									<input
+										type='text'
+										name='Name'
+										className={style.searchInputForm}
+										placeholder='your national id number *'
+										value={nationaId}
+										onChange={handleNationalIdChange}
+									/>
+								</div>
 
-                <div className={style.searchButton} onClick={onSubmitSearchNationalId}>
-                  <button className="btn btn-success">Search</button>
-                </div>
-              </div>
-            </CardLayout>
+								<div
+									className={style.searchButton}
+									onClick={onSubmitSearchNationalId}
+								>
+									<button className='btn btn-success'>Search</button>
+								</div>
+							</div>
+						</CardLayout>
 
-            {nationalIdDetails && (
-              <CardLayout>
-                <div className={style.searchResultContainer}>
-                  <div className={style.imageDesign}>
-                    <img src={nationalIdDetails?.photo} />
-                  </div>
+						{nationalIdDetails && (
+							<CardLayout>
+								<div className={style.searchResultContainer}>
+									<div className={style.imageDesign}>
+										<img src={nationalIdDetails?.photo} />
+									</div>
 
-                  <div className={style.nationalIdDetails}>
-                    <p>First Name:{nationalIdDetails?.firstName}</p>
-                    <p>Last Name:{nationalIdDetails?.lastName}</p>
+									<div className={style.nationalIdDetails}>
+										<p>First Name:{nationalIdDetails?.firstName}</p>
+										<p>Last Name:{nationalIdDetails?.lastName}</p>
+										<p>Id Number:{nationalIdDetails?.nationalIdNumber}</p>
+										<p>Date of Birth:{nationalIdDetails?.dateOfBirth}</p>
+										<p>City:{nationalIdDetails?.city}</p>
+										<p>Father Name:{nationalIdDetails?.fatherName}</p>
+										<p>Mother Name:{nationalIdDetails?.motherName}</p>
+										<p>Street:{nationalIdDetails?.street}</p>
+										<p>House Number:{nationalIdDetails?.houseNumber}</p>
+										<p>Postal Code:{nationalIdDetails?.postalCode}</p>
+										<p>Issued Date:{nationalIdDetails?.date}</p>
+									</div>
+								</div>
+							</CardLayout>
+						)}
 
-                    <p>Id Number:{nationalIdDetails?.nationalIdNumber}</p>
-
-                    <p>Date of Birth:{nationalIdDetails?.dateOfBirth}</p>
-
-                    <p>City:{nationalIdDetails?.city}</p>
-
-                    <p>Father Name:{nationalIdDetails?.fatherName}</p>
-                    <p>Mother Name:{nationalIdDetails?.motherName}</p>
-                    <p>Street:{nationalIdDetails?.street}</p>
-                    <p>House Number:{nationalIdDetails?.houseNumber}</p>
-                    <p>Postal Code:{nationalIdDetails?.postalCode}</p>
-                    <p>Issued Date:{nationalIdDetails?.date}</p>
-                  </div>
-                </div>
-              </CardLayout>
-            )}
-
-            {error && (
-              <CardLayout>
-                <h4>{error}</h4>
-              </CardLayout>
-            )}
-          </div>
-        </div>
-      </div>
-    </HomePageLayout>
-  );
+						{error && (
+							<CardLayout>
+								<h4>{error}</h4>
+							</CardLayout>
+						)}
+					</div>
+				</div>
+			</div>
+		</HomePageLayout>
+	);
 };
 
 export default NationalId;
