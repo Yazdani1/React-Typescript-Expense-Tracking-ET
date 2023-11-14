017f94f302781c864c020bc97f813b95592ebd0f
yazdani1 <yaz4dani@gmail.com>
2023-11-14 20:11:41 +0100

    WIP

--- client/src/pages/JobWishList/JobWishList.tsx
+++ client/src/pages/JobWishList/JobWishList.tsx
@@ -2,23 +2,25 @@ import { useEffect } from 'react';
 
 import SubscriberPageLayout from '../../layouts/SubscriberPageLayout';
 import { useJobWishListContext } from '../../contextapi/JobWishListContext';
-import style from './JobWishList.module.scss';
 import JobWishListCard from './JobWishListCard';
 
 const JobWishList = () => {
-  //Context api
+	const { allJobWishList, loadJobWishList } = useJobWishListContext();
 
-  const { allJobWishList, loadJobWishList } = useJobWishListContext();
+	useEffect(() => {
+		loadJobWishList();
+	}, []);
 
-  useEffect(() => {
-    loadJobWishList();
-  }, []);
-
-  return (
-    <SubscriberPageLayout>
-      <div>{allJobWishList && allJobWishList.map((job) => <JobWishListCard jobwishlist={job} key={job._id} />)}</div>
-    </SubscriberPageLayout>
-  );
+	return (
+		<SubscriberPageLayout>
+			<div>
+				{allJobWishList &&
+					allJobWishList.map((job) => (
+						<JobWishListCard jobwishlist={job} key={job._id} />
+					))}
+			</div>
+		</SubscriberPageLayout>
+	);
 };
 
 export default JobWishList;
