42232149a4af305ea3f59bb8ef5e80aab775b7a5
yazdani1 <yaz4dani@gmail.com>
2023-11-25 23:48:33 +0100

    WIP

--- client/src/pages/Company/CompanyDashboard.tsx
+++ client/src/pages/Company/CompanyDashboard.tsx
@@ -1,17 +1,15 @@
-import React from 'react';
-
 import CompanyPageLayout from '../../layouts/CompanyPageLayout';
 import CardLayout from '../../components/CardLayout/CardLayout';
 
 const CompanyDashboard = () => {
-  return (
-    <CompanyPageLayout>
-      <CardLayout>
-        <h1>Company dashboard</h1>
-        <p>This is a demo description</p>
-      </CardLayout>
-    </CompanyPageLayout>
-  );
+	return (
+		<CompanyPageLayout>
+			<CardLayout>
+				<h1>Company dashboard</h1>
+				<p>This is a demo description</p>
+			</CardLayout>
+		</CompanyPageLayout>
+	);
 };
 
 export default CompanyDashboard;
