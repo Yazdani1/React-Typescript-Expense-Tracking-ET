3259afd4246a56c9e27c11c19e0999d2007983e7
yazdani1 <yaz4dani@gmail.com>
2023-11-28 23:28:40 +0100

    WIP

--- client/src/pages/JobApplication/JobApplication.tsx
+++ client/src/pages/JobApplication/JobApplication.tsx
@@ -1,7 +1,10 @@
-import React, { FC, ReactNode, useState, useEffect } from 'react';
-import { ToastContainer, toast } from 'react-toastify';
+import { useEffect } from 'react';
 
-import { UpdateJobPostProps, updateSingleJobPost, deleteSingleJobPost } from '../../services/API';
+import {
+	UpdateJobPostProps,
+	updateSingleJobPost,
+	deleteSingleJobPost,
+} from '../../services/API';
 import { JobPosts, Visibility } from '../../services/DataProvider';
 import SubscriberPageLayout from '../../layouts/SubscriberPageLayout';
 import { useJobApplicationContext } from '../../contextapi/JobApplicationContext';
@@ -9,22 +12,25 @@ import style from './JobApplication.module.scss';
 import JobApplicationCard from './JobApplicationCard';
 
 const JobApplication = () => {
-  // Context API
+	// Context API
+	const { allJobApplication, loadJobApplication, loading } =
+		useJobApplicationContext();
 
-  const { allJobApplication, loadJobApplication, loading } = useJobApplicationContext();
+	useEffect(() => {
+		loadJobApplication();
+	}, []);
 
-  useEffect(() => {
-    loadJobApplication();
-  }, []);
-
-  return (
-    <SubscriberPageLayout>
-      <div className={style.main_container}>
-        {loading && <h3>Loading...</h3>}
-        {allJobApplication && allJobApplication.map((job) => <JobApplicationCard job_application={job} key={job._id} />)}
-      </div>
-    </SubscriberPageLayout>
-  );
+	return (
+		<SubscriberPageLayout>
+			<div className={style.main_container}>
+				{loading && <h3>Loading...</h3>}
+				{allJobApplication &&
+					allJobApplication.map((job) => (
+						<JobApplicationCard job_application={job} key={job._id} />
+					))}
+			</div>
+		</SubscriberPageLayout>
+	);
 };
 
 export default JobApplication;
