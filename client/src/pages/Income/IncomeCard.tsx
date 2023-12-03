ffc1969b5908a5b42823a0a987cf82c93d14d6ac
yazdani1 <yaz4dani@gmail.com>
2023-12-03 23:49:05 +0100

    WIP

--- client/src/pages/Income/IncomeCard.tsx
+++ client/src/pages/Income/IncomeCard.tsx
@@ -7,46 +7,46 @@ import { deleteIncomeRecord } from '../../services/API';
 import { useIncomeRecordContext } from '../../contextapi/IncomeRecordContext';
 
 interface IncomeCardProps {
-  incomeRecord: IncomeRecord;
-  //This post id is just to test if saved post opiton and button change work
-  postid: string;
+	incomeRecord: IncomeRecord;
+	//This post id is just to test if saved post opiton and button change work
+	postid: string;
 }
 
 const IncomeCard: FC<IncomeCardProps> = ({ incomeRecord, postid }) => {
-  const { allIncomeRecords, loadLogedInUserIncomeRecords } = useIncomeRecordContext();
+	const { allIncomeRecords, loadLogedInUserIncomeRecords } =
+		useIncomeRecordContext();
 
-  /****************************************/
-  /****** To delete income record  ********/
-  /****************************************/
+	/****************************************/
+	/****** To delete income record  ********/
+	/****************************************/
 
-  const deleteSingleIncomeRecord = async () => {
-    try {
-      const res = await deleteIncomeRecord(incomeRecord._id);
-      if (res) {
-        toast.success('Income record deleted successfully', {
-          position: toast.POSITION.TOP_RIGHT,
-        });
-        loadLogedInUserIncomeRecords();
-      }
-    } catch (error: any) {
-      toast.error(error.response && error.response.data.error, {
-        position: toast.POSITION.TOP_RIGHT,
-      });
-    }
-  };
-
-  return (
-    <div className={style.incomeRecordItems}>
-      <h6>{incomeRecord.title}</h6>
-      <h6>{incomeRecord.des}</h6>
-      <h6>{incomeRecord.amount}</h6>
-      <h6>{incomeRecord.date}</h6>
-      <button className="btn btn-danger" onClick={deleteSingleIncomeRecord}>
-        Delete
-      </button>
-      <p>{incomeRecord._id === postid && 'Saved'} </p>
-    </div>
-  );
+	const deleteSingleIncomeRecord = async () => {
+		try {
+			const res = await deleteIncomeRecord(incomeRecord._id);
+			if (res) {
+				toast.success('Income record deleted successfully', {
+					position: toast.POSITION.TOP_RIGHT,
+				});
+				loadLogedInUserIncomeRecords();
+			}
+		} catch (error: any) {
+			toast.error(error.response && error.response.data.error, {
+				position: toast.POSITION.TOP_RIGHT,
+			});
+		}
+	};
+	return (
+		<div className={style.incomeRecordItems}>
+			<h6>{incomeRecord.title}</h6>
+			<h6>{incomeRecord.des}</h6>
+			<h6>{incomeRecord.amount}</h6>
+			<h6>{incomeRecord.date}</h6>
+			<button className='btn btn-danger' onClick={deleteSingleIncomeRecord}>
+				Delete
+			</button>
+			<p>{incomeRecord._id === postid && 'Saved'} </p>
+		</div>
+	);
 };
 
 export default IncomeCard;
