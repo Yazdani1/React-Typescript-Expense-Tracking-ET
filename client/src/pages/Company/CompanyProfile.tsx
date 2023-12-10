6163b066c0881d057afbe23800e1d37ac5dc74ee
yazdani1 <yaz4dani@gmail.com>
2023-12-10 23:41:14 +0100

    WIP

--- client/src/pages/Company/CompanyProfile.tsx
+++ client/src/pages/Company/CompanyProfile.tsx
@@ -9,6 +9,7 @@ const CompanyProfile = () => {
 					<h2>Company Profile </h2>
 				</div>
 			</CardLayout>
+
 			<CardLayout>
 				{[1, 2, 3, 4, 5, 6, 7, 8, 9].map((item, index) => (
 					<h4>{item}.</h4>
