e0ec773a7d13f721a102cad9e67029c3ce17fff6
yazdani1 <yaz4dani@gmail.com>
2024-01-01 22:59:04 +0100

    WIP

--- client/src/pages/Employer/EmployerDashboard.tsx
+++ client/src/pages/Employer/EmployerDashboard.tsx
@@ -11,219 +11,242 @@ import style from './EmployerJobPost.module.scss';
 import ModalBox from '../../components/Modal/ModalBox';
 import TextField from '../../components/Input/TextField';
 import CardLayout from '../../components/CardLayout/CardLayout';
-import { CreateJobPostProps, createJobPosts, JobpostsVisibilityUpdateProps, updateJobPostsVisibility } from '../../services/API';
+import {
+	CreateJobPostProps,
+	createJobPosts,
+	JobpostsVisibilityUpdateProps,
+	updateJobPostsVisibility,
+} from '../../services/API';
 
 const EmployerDashboard = () => {
-  /****************************************/
-  /*********  Load Job Posts    ***********/
-  /****************************************/
+	/****************************************/
+	/*********  Load Job Posts    ***********/
+	/****************************************/
+	const [jobPosts, setJobPosts] = useState<JobPosts[]>([]);
+	const loadJobposts = async () => {
+		try {
+			const res = await getEmployerJobPosts();
 
-  const [jobPosts, setJobPosts] = useState<JobPosts[]>([]);
+			if (res) {
+				setJobPosts(res);
+			}
+		} catch (error: any) {
+			toast.error(error.response && error.response.data.error, {
+				position: toast.POSITION.TOP_RIGHT,
+			});
+		}
+	};
 
-  const loadJobposts = async () => {
-    try {
-      const res = await getEmployerJobPosts();
+	/****************************************/
+	/******Modal Box to Create Job Post   ***/
+	/****************************************/
 
-      if (res) {
-        setJobPosts(res);
-      }
-    } catch (error: any) {
-      toast.error(error.response && error.response.data.error, {
-        position: toast.POSITION.TOP_RIGHT,
-      });
-    }
-  };
+	const [open, setOpen] = useState<boolean>(false);
+	const onOpenModal = () => {
+		setOpen(true);
+	};
+	const onCloseModal = () => {
+		setOpen(false);
+	};
 
-  /****************************************/
-  /******Modal Box to Create Job Post   ***/
-  /****************************************/
+	/****************************************/
+	/****** Create job posts ****************/
+	/****************************************/
 
-  const [open, setOpen] = useState<boolean>(false);
+	const [title, setTitle] = useState<string>('');
+	const [des, setDes] = useState<string>('');
+	const [jobCity, setJobCity] = useState<string>('');
+	const [visibility, setVisibility] = useState<Visibility>(Visibility.Public);
+	const [addjobSkills, setAddJobSkills] = useState<string>('');
+	const [jobSkillsList, setJobSkillsList] = useState<string[]>([]);
 
-  const onOpenModal = () => {
-    setOpen(true);
-  };
+	const addJobSkills = () => {
+		setJobSkillsList([...jobSkillsList, addjobSkills]);
+		setAddJobSkills('');
+	};
+	const removeJobSkills = (skillIndex: string) => {
+		const skills = jobSkillsList.filter((item) => item !== skillIndex);
+		setJobSkillsList(skills);
+	};
+	const onSubmitCreateJobPosts = async () => {
+		try {
+			const payload: CreateJobPostProps = {
+				title: title,
+				des: des,
+				jobCity: jobCity,
+				jobSkills: jobSkillsList,
+				visibility: visibility,
+			};
+			const res = await createJobPosts(payload);
+			if (res) {
+				toast.success('Job post created successfully!', {
+					position: toast.POSITION.TOP_RIGHT,
+				});
+				loadJobposts();
+				onResetJobPostInputField();
+			}
+		} catch (error: any) {
+			toast.error(error.response && error.response.data.error, {
+				position: toast.POSITION.TOP_RIGHT,
+			});
+		}
+	};
 
-  const onCloseModal = () => {
-    setOpen(false);
-  };
+	const onResetJobPostInputField = () => {
+		setTitle('');
+		setDes('');
+		setJobCity('');
+		setVisibility(Visibility.Public);
+		setAddJobSkills('');
+		setJobSkillsList([]);
+	};
 
-  /****************************************/
-  /****** Create job posts ****************/
-  /****************************************/
+	/****************************************/
+	/* Update all visibility for job post ***/
+	/****************************************/
 
-  const [title, setTitle] = useState<string>('');
-  const [des, setDes] = useState<string>('');
-  const [jobCity, setJobCity] = useState<string>('');
-  const [visibility, setVisibility] = useState<Visibility>(Visibility.Public);
-  const [addjobSkills, setAddJobSkills] = useState<string>('');
-  const [jobSkillsList, setJobSkillsList] = useState<string[]>([]);
+	const [updateJobVisibility, setUpdateJobVisibility] = useState<Visibility>(
+		Visibility.Public
+	);
 
-  const addJobSkills = () => {
-    setJobSkillsList([...jobSkillsList, addjobSkills]);
-    setAddJobSkills('');
-  };
+	const handleUpdateJobVisibility = async () => {
+		try {
+			const payload: JobpostsVisibilityUpdateProps = {
+				updateVisibility: updateJobVisibility,
+			};
 
-  const removeJobSkills = (skillIndex: string) => {
-    const skills = jobSkillsList.filter((item) => item !== skillIndex);
-    setJobSkillsList(skills);
-  };
+			const res = await updateJobPostsVisibility(payload);
 
-  const onSubmitCreateJobPosts = async () => {
-    try {
-      const payload: CreateJobPostProps = {
-        title: title,
-        des: des,
-        jobCity: jobCity,
-        jobSkills: jobSkillsList,
-        visibility: visibility,
-      };
+			if (res) {
+				toast.success('Job post visibility updated successfully!', {
+					position: toast.POSITION.TOP_RIGHT,
+				});
+				loadJobposts();
+			}
+		} catch (error: any) {
+			toast.error(error.response && error.response.data.error, {
+				position: toast.POSITION.TOP_RIGHT,
+			});
+		}
+	};
 
-      const res = await createJobPosts(payload);
-      if (res) {
-        toast.success('Job post created successfully!', {
-          position: toast.POSITION.TOP_RIGHT,
-        });
-        loadJobposts();
-        onResetJobPostInputField();
-      }
-    } catch (error: any) {
-      toast.error(error.response && error.response.data.error, {
-        position: toast.POSITION.TOP_RIGHT,
-      });
-    }
-  };
+	useEffect(() => {
+		loadJobposts();
+	}, []);
 
-  const onResetJobPostInputField = () => {
-    setTitle('');
-    setDes('');
-    setJobCity('');
-    setVisibility(Visibility.Public);
-    setAddJobSkills('');
-    setJobSkillsList([]);
-  };
+	return (
+		<EmployerPageLayout>
+			<h1>Employer dashboar</h1>
 
-  /****************************************/
-  /* Update all visibility for job post ***/
-  /****************************************/
+			<CardLayout>
+				<button className='btn btn-success' onClick={onOpenModal}>
+					Create job post
+				</button>
+			</CardLayout>
 
-  const [updateJobVisibility, setUpdateJobVisibility] = useState<Visibility>(Visibility.Public);
+			{/* To update all the job visibility */}
+			<CardLayout>
+				<div className={style.updateVisibilitycontainer}>
+					<div className={style.visibility_drop_down_container}>
+						<select
+							value={updateJobVisibility}
+							className={style.visiblity_item}
+							onChange={(e) =>
+								setUpdateJobVisibility(e.target.value as Visibility)
+							}
+						>
+							{Object.keys(Visibility).map((vtype) => (
+								<option value={vtype}>{vtype}</option>
+							))}
+						</select>
+					</div>
+					<button
+						className={style.updateVisibilityButton}
+						onClick={handleUpdateJobVisibility}
+					>
+						Update all visibility
+					</button>
+				</div>
+			</CardLayout>
 
-  const handleUpdateJobVisibility = async () => {
-    try {
-      const payload: JobpostsVisibilityUpdateProps = {
-        updateVisibility: updateJobVisibility,
-      };
+			<JobPostItemRow />
+			<div style={{ height: '100vh' }}>
+				{jobPosts &&
+					jobPosts.map((jobpost) => (
+						<EmployerJobPostCard
+							jobpost={jobpost}
+							key={jobpost._id}
+							loadJobposts={loadJobposts}
+						/>
+					))}
+			</div>
 
-      const res = await updateJobPostsVisibility(payload);
+			{/* Modal box to create jobpost */}
+			<ModalBox
+				open={open}
+				onCloseModal={onCloseModal}
+				title='Create job posts'
+				onResetButton={onResetJobPostInputField}
+				onSaveButton={onSubmitCreateJobPosts}
+			>
+				<TextField label='Title' value={title} setValue={setTitle} />
+				<TextField label='City' value={jobCity} setValue={setJobCity} />
+				<TextField label='Description' value={des} setValue={setDes} />
 
-      if (res) {
-        toast.success('Job post visibility updated successfully!', {
-          position: toast.POSITION.TOP_RIGHT,
-        });
-        loadJobposts();
-      }
-    } catch (error: any) {
-      toast.error(error.response && error.response.data.error, {
-        position: toast.POSITION.TOP_RIGHT,
-      });
-    }
-  };
+				<div className={style.visibility_drop_down_container}>
+					<select
+						value={visibility}
+						className={style.visiblity_item}
+						onChange={(e) => setVisibility(e.target.value as Visibility)}
+					>
+						{Object.keys(Visibility).map((vtype) => (
+							<option value={vtype}>{vtype}</option>
+						))}
+					</select>
+				</div>
 
-  useEffect(() => {
-    loadJobposts();
-  }, []);
+				<TextField
+					label='Job skills'
+					value={addjobSkills}
+					setValue={setAddJobSkills}
+				/>
 
-  return (
-    <EmployerPageLayout>
-      <h1>Employer dashboar</h1>
+				<div className={style.skills_container}>
+					{jobSkillsList.map((skill, index) => (
+						<div key={index} className={style.skill_item}>
+							<p className={style.skill}>{skill}</p>
+							<p
+								className={style.close_icon}
+								onClick={() => removeJobSkills(skill)}
+							>
+								<IoMdClose />
+							</p>
+						</div>
+					))}
+				</div>
 
-      <CardLayout>
-        <button className="btn btn-success" onClick={onOpenModal}>
-          Create job post
-        </button>
-      </CardLayout>
-
-      {/* To update all the job visibility */}
-      <CardLayout>
-        <div className={style.updateVisibilitycontainer}>
-          <div className={style.visibility_drop_down_container}>
-            <select
-              value={updateJobVisibility}
-              className={style.visiblity_item}
-              onChange={(e) => setUpdateJobVisibility(e.target.value as Visibility)}
-            >
-              {Object.keys(Visibility).map((vtype) => (
-                <option value={vtype}>{vtype}</option>
-              ))}
-            </select>
-          </div>
-          <button className={style.updateVisibilityButton} onClick={handleUpdateJobVisibility}>
-            Update all visibility
-          </button>
-        </div>
-      </CardLayout>
-
-      <JobPostItemRow />
-      <div style={{ height: '100vh' }}>
-        {jobPosts && jobPosts.map((jobpost) => <EmployerJobPostCard jobpost={jobpost} key={jobpost._id} loadJobposts={loadJobposts} />)}
-      </div>
-
-      {/* Modal box to create jobpost */}
-      <ModalBox
-        open={open}
-        onCloseModal={onCloseModal}
-        title="Create job posts"
-        onResetButton={onResetJobPostInputField}
-        onSaveButton={onSubmitCreateJobPosts}
-      >
-        <TextField label="Title" value={title} setValue={setTitle} />
-        <TextField label="City" value={jobCity} setValue={setJobCity} />
-        <TextField label="Description" value={des} setValue={setDes} />
-
-        <div className={style.visibility_drop_down_container}>
-          <select value={visibility} className={style.visiblity_item} onChange={(e) => setVisibility(e.target.value as Visibility)}>
-            {Object.keys(Visibility).map((vtype) => (
-              <option value={vtype}>{vtype}</option>
-            ))}
-          </select>
-        </div>
-
-        <TextField label="Job skills" value={addjobSkills} setValue={setAddJobSkills} />
-
-        <div className={style.skills_container}>
-          {jobSkillsList.map((skill, index) => (
-            <div key={index} className={style.skill_item}>
-              <p className={style.skill}>{skill}</p>
-              <p className={style.close_icon} onClick={() => removeJobSkills(skill)}>
-                <IoMdClose />
-              </p>
-            </div>
-          ))}
-        </div>
-
-        <button className="btn btn-warning" onClick={addJobSkills}>
-          Add Skills
-        </button>
-      </ModalBox>
-    </EmployerPageLayout>
-  );
+				<button className='btn btn-warning' onClick={addJobSkills}>
+					Add Skills
+				</button>
+			</ModalBox>
+		</EmployerPageLayout>
+	);
 };
 
 export default EmployerDashboard;
 
 const JobPostItemRow = () => {
-  return (
-    <div className={style.jobpost_header_row}>
-      <h6>Title</h6>
-      <p>Visibility</p>
-      <p>Status</p>
-      <p>City</p>
-      <p>Application</p>
-      {/* <p>Edit</p>
+	return (
+		<div className={style.jobpost_header_row}>
+			<h6>Title</h6>
+			<p>Visibility</p>
+			<p>Status</p>
+			<p>City</p>
+			<p>Application</p>
+			{/* <p>Edit</p>
       <p>Delete</p> */}
-      <p>Preview</p>
-      <p>Action</p>
-    </div>
-  );
+			<p>Preview</p>
+			<p>Action</p>
+		</div>
+	);
 };
