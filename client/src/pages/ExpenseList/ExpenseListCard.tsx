ddfa0c4e5d88d8615cf8d42cc592b58b7b5b1c1d
yazdani1 <yaz4dani@gmail.com>
2024-01-08 20:42:10 +0100

    WIP

--- client/src/pages/ExpenseList/ExpenseListCard.tsx
+++ client/src/pages/ExpenseList/ExpenseListCard.tsx
@@ -5,28 +5,27 @@ import expenseListCardStyle from './ExpenseListCard.module.scss';
 import { ExpenseList } from '../../services/DataProvider';
 
 interface ExpenseListCardProps {
-  expenseList: ExpenseList;
+	expenseList: ExpenseList;
 }
-
 const ExpenseListCard: FC<ExpenseListCardProps> = ({ expenseList }) => {
-  return (
-    <div className={expenseListCardStyle.expenseListCardContainer}>
-      <div className="row">
-        <div className="col-xl-3 col-lg-2 col-md-12 col-sm-6">
-          <h6>{expenseList.title}</h6>
-        </div>
-        <div className="col-xl-3 col-lg-2 col-md-12 col-sm-6">
-          <h6>{expenseList.amount}</h6>
-        </div>
-        <div className="col-xl-3 col-lg-2 col-md-12 col-sm-6">
-          <h6>{expenseList.expense_category}</h6>
-        </div>
-        <div className="col-xl-3 col-lg-2 col-md-12 col-sm-6">
-          <h6> {moment(expenseList.date).format('MMM Do YY')}</h6>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className={expenseListCardStyle.expenseListCardContainer}>
+			<div className='row'>
+				<div className='col-xl-3 col-lg-2 col-md-12 col-sm-6'>
+					<h6>{expenseList.title}</h6>
+				</div>
+				<div className='col-xl-3 col-lg-2 col-md-12 col-sm-6'>
+					<h6>{expenseList.amount}</h6>
+				</div>
+				<div className='col-xl-3 col-lg-2 col-md-12 col-sm-6'>
+					<h6>{expenseList.expense_category}</h6>
+				</div>
+				<div className='col-xl-3 col-lg-2 col-md-12 col-sm-6'>
+					<h6> {moment(expenseList.date).format('MMM Do YY')}</h6>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default ExpenseListCard;
