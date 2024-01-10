7d42dde383bea5cd555ad9bbc22c53d916fcfe65
yazdani1 <yaz4dani@gmail.com>
2024-01-10 23:57:37 +0100

    WIP

--- client/src/pages/JobWishList/JobWishListCard.tsx
+++ client/src/pages/JobWishList/JobWishListCard.tsx
@@ -10,13 +10,14 @@ import style from './JobWishList.module.scss';
 interface JobWishListCardProps {
 	jobwishlist: JobWishList;
 }
-
 const JobWishListCard: FC<JobWishListCardProps> = ({ jobwishlist }) => {
 	//Context api
 	const { allJobWishList, loadJobWishList } = useJobWishListContext();
+
 	/****************************************/
 	/****** Delete job wishlist *************/
 	/****************************************/
+
 	const handleDeleteJobWishList = async () => {
 		try {
 			const res = await deleteJobWishList(jobwishlist?._id);
@@ -37,6 +38,7 @@ const JobWishListCard: FC<JobWishListCardProps> = ({ jobwishlist }) => {
 			<h6>{jobwishlist.jobPostId?.title}</h6>
 			<p>Published by:{jobwishlist.jobPostPublishedBy?.name}</p>
 			<p>{jobwishlist.jobPostId?.des}</p>
+			
 			<div className={style.job_skills}>
 				{jobwishlist.jobPostId?.jobSkills?.map((skill, index) => (
 					<p key={index} className={style.skill_item}>
@@ -44,6 +46,7 @@ const JobWishListCard: FC<JobWishListCardProps> = ({ jobwishlist }) => {
 					</p>
 				))}
 			</div>
+
 			<div className={style.job_post_bottom_row}>
 				<p>{jobwishlist.jobPostId?.jobCity}</p>
 				<p>{jobwishlist.jobPostId?.visibility}</p>
