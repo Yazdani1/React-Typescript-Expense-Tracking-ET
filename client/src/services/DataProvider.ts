30aafcfb62ab49c4e3c1381c8d2536d877cd0a7c
yazdani1 <yaz4dani@gmail.com>
2024-01-14 23:50:44 +0100

    WIP

--- client/src/services/DataProvider.ts
+++ client/src/services/DataProvider.ts
@@ -78,17 +78,14 @@ export enum ExpenseBookColor {
 // This to show in the expense book detials and for other properties.
 // Cause when visit the detials page it returns other inof as well
 // This one will have to add in the API.ts file for this funciton getExpenseBookDetails();--panding
-
 export interface ExpenseBookDetails {
 	singleExpenseBook: ExpenseBookInfo;
 	expenseBookCategory: ExpenseCategory[];
 	expenseList: ExpenseList[];
 }
-
 /****************************************/
 /* Expense Book Category, ExpenseList   */
 /****************************************/
-
 export interface ExpenseCategory extends IBase {
 	category_name: string;
 	expense_book_id: string;
@@ -105,11 +102,9 @@ export interface ExpenseCountByCategory {
 	_id: string;
 	totalammount: number;
 }
-
 /****************************************/
 /*** Chart Types                   ******/
 /****************************************/
-
 export enum TotalExpenseChartTypes {
 	Line_Chart = 'Line_Chart',
 	Area_Chart = 'Area_Chart',
