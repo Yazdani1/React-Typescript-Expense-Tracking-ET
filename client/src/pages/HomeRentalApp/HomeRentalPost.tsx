d75c34ac4a880a58501822c79ebd758e5b98ee90
yazdani1 <yaz4dani@gmail.com>
2024-01-22 22:05:52 +0100

    WIP

--- client/src/pages/HomeRentalApp/HomeRentalPost.tsx
+++ client/src/pages/HomeRentalApp/HomeRentalPost.tsx
@@ -7,7 +7,6 @@ const HomeRentalPost = () => {
 	// Here I am using context api to show user data and home rental post.
 	// Here i need to call the useHomeRentalContext function that is connected with the useContext in the contex api
 	// here i just need to use all the api data that passed then i can use it here.
-
 	const { allHomeRentPosts, allUsers } = useHomeRentalContext();
 	const { allIncomeRecords } = useIncomeRecordContext();
 
@@ -22,8 +21,10 @@ const HomeRentalPost = () => {
 								<h6>{income.title}</h6>
 							))}
 					</CardLayout>
+
 					<CardLayout>
 						<p>HomeRentalPost. {allHomeRentPosts.length}</p>
+
 						<div>
 							{allHomeRentPosts &&
 								allHomeRentPosts.map((post: any) => (
