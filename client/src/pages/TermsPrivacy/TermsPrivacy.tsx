d32b592691f4523ce58fd4cb0c4fce5daa393bc9
yazdani1 <yaz4dani@gmail.com>
2024-02-22 23:45:43 +0100

    WIP

--- client/src/pages/TermsPrivacy/TermsPrivacy.tsx
+++ client/src/pages/TermsPrivacy/TermsPrivacy.tsx
@@ -6,6 +6,7 @@ const TermsPrivacy = () => {
 			<p>{`Type: ${subSection.type}, Content: ${subSection.content}`}</p>
 		</div>
 	);
+
 	const renderSection = (section: any, level: number) => (
 		<div key={section.type} style={{ marginLeft: `${level * 20}px` }}>
 			<h3>{section.content}</h3>
@@ -19,6 +20,7 @@ const TermsPrivacy = () => {
 			)}
 		</div>
 	);
+
 	return (
 		<div>
 			<h1>{data.title}</h1>
