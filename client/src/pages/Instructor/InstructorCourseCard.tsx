2d22da68e65ce88372fd53fcaddabe922325ce3c
yazdani1 <yaz4dani@gmail.com>
2024-04-06 21:22:40 +0200

    WIP

--- client/src/pages/Instructor/InstructorCourseCard.tsx
+++ client/src/pages/Instructor/InstructorCourseCard.tsx
@@ -5,26 +5,24 @@ import { Course } from '../../services/DataProvider';
 import style from './InstructorDashboard.module.scss';
 
 interface InstructorCourseCardProps {
-  course: Course;
+	course: Course;
 }
 const InstructorCourseCard: FC<InstructorCourseCardProps> = ({ course }) => {
-  return (
-    <Link to={'/instructor-dashboard/course-details/' + course.slug} style={{ textDecoration: 'none', color: 'inherit' }}>
-      <div className={style.courseContainer}>
-
-        <h6>{course.title}</h6>
-
-        <p>{course.des}</p>
-
-        <div className={style.itemRow}>
-
-          <p>Enrolled:{course.enrolledStudents}</p>
-          
-          <p>Seats:{course.maxStudents}</p>
-        </div>
-      </div>
-    </Link>
-  );
+	return (
+		<Link
+			to={'/instructor-dashboard/course-details/' + course.slug}
+			style={{ textDecoration: 'none', color: 'inherit' }}
+		>
+			<div className={style.courseContainer}>
+				<h6>{course.title}</h6>
+				<p>{course.des}</p>
+				<div className={style.itemRow}>
+					<p>Enrolled:{course.enrolledStudents}</p>
+					<p>Seats:{course.maxStudents}</p>
+				</div>
+			</div>
+		</Link>
+	);
 };
 
 export default InstructorCourseCard;
