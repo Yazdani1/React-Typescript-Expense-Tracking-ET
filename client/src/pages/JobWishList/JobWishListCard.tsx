f9da8c9bbb9e1664be19001f0182d6f5620381f0
yazdani1 <yaz4dani@gmail.com>
2024-05-26 23:48:14 +0200

    WIP

--- client/src/pages/JobWishList/JobWishListCard.tsx
+++ client/src/pages/JobWishList/JobWishListCard.tsx
@@ -10,8 +10,10 @@ import style from './JobWishList.module.scss';
 interface JobWishListCardProps {
 	jobwishlist: JobWishList;
 }
+
 const JobWishListCard: FC<JobWishListCardProps> = ({ jobwishlist }) => {
 	//Context api
+
 	const { allJobWishList, loadJobWishList } = useJobWishListContext();
 
 	/****************************************/
@@ -33,12 +35,12 @@ const JobWishListCard: FC<JobWishListCardProps> = ({ jobwishlist }) => {
 			});
 		}
 	};
+
 	return (
 		<div className={style.job_post_container}>
 			<h6>{jobwishlist.jobPostId?.title}</h6>
 			<p>Published by:{jobwishlist.jobPostPublishedBy?.name}</p>
 			<p>{jobwishlist.jobPostId?.des}</p>
-
 			<div className={style.job_skills}>
 				{jobwishlist.jobPostId?.jobSkills?.map((skill, index) => (
 					<p key={index} className={style.skill_item}>
@@ -46,7 +48,6 @@ const JobWishListCard: FC<JobWishListCardProps> = ({ jobwishlist }) => {
 					</p>
 				))}
 			</div>
-
 			<div className={style.job_post_bottom_row}>
 				<p>{jobwishlist.jobPostId?.jobCity}</p>
 				<p>{jobwishlist.jobPostId?.visibility}</p>
