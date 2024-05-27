c7935eacb3b93b9b47386153a13b1aa0a149ded2
yazdani1 <yaz4dani@gmail.com>
2024-05-27 23:56:28 +0200

    WIP

--- client/src/pages/JobWishList/JobWishListCard.tsx
+++ client/src/pages/JobWishList/JobWishListCard.tsx
@@ -23,6 +23,7 @@ const JobWishListCard: FC<JobWishListCardProps> = ({ jobwishlist }) => {
 	const handleDeleteJobWishList = async () => {
 		try {
 			const res = await deleteJobWishList(jobwishlist?._id);
+
 			if (res) {
 				toast.success('Job post deleted from your wishlist', {
 					position: toast.POSITION.TOP_CENTER,
